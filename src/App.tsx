34e4e3eda70bab6447020ea364b5855ef4479d3a
[Aman Thakur] <unknnown1111@gmail.com>
2025-04-04 01:18:05 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React , { useState} from 'react';
 import './App.css';
 import Block from './components/Block';
 
 function App() {
+
+  const [state, setState] = React.useState(Array(9).fill(null));
+  const [currentTurn,setCurrentTurn]= useState("x")
+  const handleBlockClick = (index:number) =>{
+    const stateCopy = Array.from(state);
+    state[index]= currentTurn;
+    setCurrentTurn(currentTurn ==="X" ? "O" : "X");
+    setState(stateCopy);
+  }
+
   return (
     <div className="board">
       <div className="row">
-        <Block />
-        <Block />
-        <Block />
+        <Block onClick= {() => handleBlockClick(0)} value={state[0]} />
+        <Block onClick= {() => handleBlockClick(1)} value={state[1]} />
+        <Block onClick= {() => handleBlockClick(2)} value={state[2]} />
       </div>
       <div className="row">
-        <Block />
-        <Block />
-        <Block />
+      <Block onClick= {() => handleBlockClick(3)} value={state[3]} />
+      <Block onClick= {() => handleBlockClick(4)} value={state[4]} />
+      <Block onClick= {() => handleBlockClick(5)} value={state[5]} />
       </div>
       <div className="row">
-        <Block />
-        <Block />
-        <Block />
+      <Block onClick= {() => handleBlockClick(6)} value={state[6]} />
+      <Block onClick= {() => handleBlockClick(7)} value={state[7]} />
+      <Block onClick= {() => handleBlockClick(8)} value={state[8]} />
       </div>
     </div>
   );
--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import '../App.css';
 
-const Block: React.FC = () => {
+interface BlockProps {
+   value: string;
+   onClick?: () => void;
+}
+
+const Block: React.FC<BlockProps>=(props)=>{
     return(
-        <div className="block"></div>
+        <div onClick={props.onClick} className = "block">
+            {props.value}
+        </div>
     )
 }
 
