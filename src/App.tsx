27c433cbf50d117720d3ca8466aebd066f8d7718
[Aman Thakur] <unknnown1111@gmail.com>
2025-04-07 08:12:13 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,61 @@
-import React , { useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Block from './components/Block';
 
 function App() {
+  const [state, setState] = useState(Array(9).fill(null));
+  const [currentTurn, setCurrentTurn] = useState("X");
 
-  const [state, setState] = React.useState(Array(9).fill(null));
-  const [currentTurn,setCurrentTurn]= useState("x")
-  const handleBlockClick = (index:number) =>{
-    const stateCopy = Array.from(state);
-    state[index]= currentTurn;
-    setCurrentTurn(currentTurn ==="X" ? "O" : "X");
-    setState(stateCopy);
+const checkWinner = (state:any[]) => {
+  const win = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+
+    [0,4,8],
+    [2,4,6]
+  ];
+
+  for (let i=0; i <win.length; i++){
+    const [a,b,c]=win[i];
+    if (state[a] !== null && state[a] === state[b] && state[a]===state[c]) return true;
+  }
+  return false;
+}
+
+  const handleBlockClick = (index: number) => {
+    
+    if (state[index] === null) {
+      const stateCopy = state.map((value, idx) => idx === index ? currentTurn : value);
+      setState(stateCopy);
+      setCurrentTurn(currentTurn === "X" ? "O" : "X");
+      const win = checkWinner(stateCopy);
+      if(win){
+       alert(`Player ${currentTurn} wins!`);
+      }
+    }
   }
 
   return (
     <div className="board">
       <div className="row">
-        <Block onClick= {() => handleBlockClick(0)} value={state[0]} />
-        <Block onClick= {() => handleBlockClick(1)} value={state[1]} />
-        <Block onClick= {() => handleBlockClick(2)} value={state[2]} />
+        <Block onClick={() => handleBlockClick(0)} value={state[0]} />
+        <Block onClick={() => handleBlockClick(1)} value={state[1]} />
+        <Block onClick={() => handleBlockClick(2)} value={state[2]} />
       </div>
       <div className="row">
-      <Block onClick= {() => handleBlockClick(3)} value={state[3]} />
-      <Block onClick= {() => handleBlockClick(4)} value={state[4]} />
-      <Block onClick= {() => handleBlockClick(5)} value={state[5]} />
+        <Block onClick={() => handleBlockClick(3)} value={state[3]} />
+        <Block onClick={() => handleBlockClick(4)} value={state[4]} />
+        <Block onClick={() => handleBlockClick(5)} value={state[5]} />
       </div>
       <div className="row">
-      <Block onClick= {() => handleBlockClick(6)} value={state[6]} />
-      <Block onClick= {() => handleBlockClick(7)} value={state[7]} />
-      <Block onClick= {() => handleBlockClick(8)} value={state[8]} />
+        <Block onClick={() => handleBlockClick(6)} value={state[6]} />
+        <Block onClick={() => handleBlockClick(7)} value={state[7]} />
+        <Block onClick={() => handleBlockClick(8)} value={state[8]} />
       </div>
     </div>
   );
